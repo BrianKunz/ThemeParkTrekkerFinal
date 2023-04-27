99c0f36975222f9cd226e07e316747545e9de1bc
randomstump <kunzie1122@gmail.com>
2023-04-27 14:27:18 -0400

    recreated dist folder

--- dist/src/controllers/user.controller.d.ts
+++
@@ -1,2 +0,0 @@
-declare const userController: import("express-serve-static-core").Router;
-export default userController;
--- dist/src/entities/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request as ExpressRequest } from "express";
-import { User } from "./User.entity";
-export type Request = ExpressRequest & {
-    user?: User;
-};
