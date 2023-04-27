b5e04077f3ad0c5f48a354e9d5eb2a392e0f869b
randomstump <kunzie1122@gmail.com>
2023-04-27 01:55:05 -0400

    updated app to fix error with switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { Switch } from "react-dom";
+// import { Switch } from "react-dom";
 import TripList from "./components/Trip/TripList";
 import PostList from "./components/Post/PostList";
 import CreateUser from "./components/User/CreateUser/CreateUser";
@@ -11,15 +11,13 @@ import HomePage from "./components/Home";
 const App: React.FC = () => {
   return (
     <main className="App">
-      <Switch>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/trips" element={<TripList />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<Post />} />
-        <Route path="/signup" element={<CreateUser />} />
-        <Route path="/login" element={<LoginUser />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Switch>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/trips" element={<TripList />} />
+      <Route path="/posts" element={<PostList />} />
+      <Route path="/posts/:id" element={<Post />} />
+      <Route path="/signup" element={<CreateUser />} />
+      <Route path="/login" element={<LoginUser />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </main>
   );
 };
