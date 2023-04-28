b4e8dab1f45efb5c2e17ed879beaf363635a6555
randomstump <kunzie1122@gmail.com>
2023-04-28 05:58:55 -0400

    udpated index.tsx

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,7 +4,9 @@ const tslib_1 = require("tslib");
 const react_1 = tslib_1.__importDefault(require("react"));
 const client_1 = tslib_1.__importDefault(require("react-dom/client"));
 const App_1 = tslib_1.__importDefault(require("./App"));
-const root = client_1.default.createRoot(document.getElementById("root"));
-root.render(react_1.default.createElement(react_1.default.StrictMode, null,
-    react_1.default.createElement(App_1.default, null)));
+if (typeof window !== "undefined") {
+    const root = client_1.default.createRoot(document.getElementById("root"));
+    root.render(react_1.default.createElement(react_1.default.StrictMode, null,
+        react_1.default.createElement(App_1.default, null)));
+}
 //# sourceMappingURL=index.js.map
