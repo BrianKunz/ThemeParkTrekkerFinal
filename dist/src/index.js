d955a5f5b82f33c11481b0ad493c9b8f952c929a
randomstump <kunzie1122@gmail.com>
2023-04-28 03:57:49 -0400

    removed css file

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,7 +4,6 @@ const tslib_1 = require("tslib");
 const react_1 = tslib_1.__importDefault(require("react"));
 const client_1 = tslib_1.__importDefault(require("react-dom/client"));
 const App_1 = tslib_1.__importDefault(require("./App"));
-require("./index.css");
 const react_router_dom_1 = require("react-router-dom");
 require("tslib");
 client_1.default.createRoot(document.getElementById("root")).render(react_1.default.createElement(react_1.default.StrictMode, null,
