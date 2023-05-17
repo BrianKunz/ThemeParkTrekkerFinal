f7fe8635e6acd76da7dbd031571a139c02f7a9e5
randomstump <kunzie1122@gmail.com>
2023-05-16 21:54:32 -0400

    updated getCookie function

--- src/services/tripService.ts
+++ src/services/tripService.ts
@@ -63,10 +63,9 @@ export const tripService = {
 };
 
 function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    return parts.pop()?.split(";").shift();
+  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
+  if (match) {
+    return match[2];
   }
   return null;
 }
