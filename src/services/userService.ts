8f9d23be8b9425853ec66408cb25b777a6e3ef10
randomstump <kunzie1122@gmail.com>
2023-05-16 22:21:09 -0400

    updated options to set token properly and allow for retrieval

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -50,7 +50,7 @@ export const userService = {
     const userId = response.data.user.id;
 
     // Set the token in a cookie
-    const options = { httpOnly: true };
+    const options = { httpOnly: false };
     const cookies = Cookies.serialize("accessToken", token, options);
     document.cookie = cookies;
 
