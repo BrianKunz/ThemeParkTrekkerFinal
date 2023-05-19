447ce9a2936166a591814bcdfec849aba7826c42
randomstump <kunzie1122@gmail.com>
2023-05-18 20:46:12 -0400

    updated controller to test user correction

--- src/controllers/trip.controller.ts
+++ src/controllers/trip.controller.ts
@@ -20,34 +20,26 @@ const authorize = async (
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  console.log(authHeader);
 
   const token = authHeader.split(" ")[1];
   const secret = process.env.JWT_SECRET || "default-secret";
-  console.log(token);
 
   try {
-    // Decode the token and extract the user ID
-    const decodedToken = jwt.verify(token, secret);
-    if (typeof decodedToken === "object" && decodedToken.userId) {
-      const userId = decodedToken.userId;
+    const decodedToken = jwt.verify(token, secret) as { userId: number };
+    const userId = decodedToken.userId;
 
-      // Query the users table to check if the user exists
-      const { rows } = await pool.query<User>(
-        "SELECT * FROM users WHERE id = $1",
-        [userId]
-      );
-      const user = rows[0];
-      if (!user) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
+    const { rows } = await pool.query<User>(
+      "SELECT * FROM users WHERE id = $1",
+      [userId]
+    );
+    const user = rows[0];
 
-      // Set the req.user property and call the next middleware function
-      req.user = user;
-      return next();
-    } else {
+    if (!user) {
       return res.status(401).json({ message: "Unauthorized" });
     }
+
+    req.user = user;
+    return next();
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal Server Error" });
@@ -56,9 +48,8 @@ const authorize = async (
 
 // Index
 tripController.get("/", authorize, async (req: AuthRequest, res: Response) => {
-  const userId = req.user?.id;
-
   try {
+    const userId = req.user!.id; // Assuming user ID is required and available
     const { rows } = await pool.query<Trip>(
       "SELECT * FROM trips WHERE user_id = $1",
       [userId]
@@ -66,10 +57,7 @@ tripController.get("/", authorize, async (req: AuthRequest, res: Response) => {
     res.json(rows);
   } catch (error) {
     console.error(error);
-    if (!res.headersSent) {
-      // check if headers have been sent
-      res.status(500).json({ message: "Internal Server Error" });
-    }
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
@@ -79,16 +67,19 @@ tripController.get(
   authorize,
   async (req: AuthRequest, res: Response) => {
     const { id } = req.params;
+    const userId = req.user!.id; // Assuming user ID is required and available
 
     try {
       const { rows } = await pool.query<Trip>(
         "SELECT * FROM trips WHERE id = $1 AND user_id = $2",
-        [id, req.user?.id]
+        [id, userId]
       );
       const trip = rows[0];
+
       if (!trip) {
         return res.status(404).json({ message: "Not Found" });
       }
+
       res.json(trip);
     } catch (error) {
       console.error(error);
@@ -101,7 +92,7 @@ tripController.get(
 // Create
 tripController.post("/", authorize, async (req: AuthRequest, res: Response) => {
   const { title, date, start_date, end_date, flight } = req.body;
-  const userId = req.user?.id;
+  const userId = req.user!.id; // Assuming user ID is required and available
 
   try {
     const { rows: userRows } = await pool.query<User>(
@@ -109,9 +100,11 @@ tripController.post("/", authorize, async (req: AuthRequest, res: Response) => {
       [userId]
     );
     const user = userRows[0];
+
     if (!user) {
       return res.status(404).json({ message: "User Not Found" });
     }
+
     const { rows } = await pool.query<Trip>(
       "INSERT INTO trips (title, date, start_date, end_date, flight, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
       [title, date, start_date, end_date, flight, userId]
@@ -129,15 +122,17 @@ tripController.put(
   "/:id",
   authorize,
   async (req: AuthRequest, res: Response) => {
-    const { title, date, start_date, end_date, flight } = req.body;
     const { id } = req.params;
+    const { title, date, start_date, end_date, flight } = req.body;
+    const userId = req.user!.id; // Assuming user ID is required and available
 
     try {
       const { rows } = await pool.query<Trip>(
         "SELECT * FROM trips WHERE id = $1 AND user_id = $2",
-        [id, req.user?.id]
+        [id, userId]
       );
       const trip = rows[0];
+
       if (!trip) {
         return res.status(404).json({ message: "Not Found" });
       }
@@ -146,6 +141,7 @@ tripController.put(
         "UPDATE trips SET title = $1, date = $2, start_date = $3, end_date = $4, flight = $5 WHERE id = $6 RETURNING *",
         [title, date, start_date, end_date, flight, id]
       );
+
       res.json(result.rows[0]);
     } catch (error) {
       console.error(error);
@@ -161,13 +157,15 @@ tripController.delete(
   authorize,
   async (req: AuthRequest, res: Response) => {
     const { id } = req.params;
+    const userId = req.user!.id; // Assuming user ID is required and available
 
     try {
       const { rows } = await pool.query<Trip>(
         "SELECT * FROM trips WHERE id = $1 AND user_id = $2",
-        [id, req.user?.id]
+        [id, userId]
       );
       const trip = rows[0];
+
       if (!trip) {
         return res.status(404).json({ message: "Not Found" });
       }
